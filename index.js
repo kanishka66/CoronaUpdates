54c96fd41983833ad50a11d80b0661fc7c333c3c
kanishka <129588726+kanishka66@users.noreply.github.com>
2023-08-15 22:33:02 +0530

    Add files via upload

---
+++ index.js
@@ -0,0 +1,19 @@
+const express = require('express');
+const app =express();
+
+// app.get('/',(req, res)=>{
+//     res.sendFile(__dirname + '/t3.html');
+  //  res.send('Hello,World');
+    // })
+    // app.get('/style.css',(req,res)=>{
+    // res.sendFile(__dirname + '/style.css');
+        
+    // })
+
+    app.get('/',(req, res)=>{
+      res.send('<h1>hello motu<h1/>')})
+
+ app.listen(8000,()=>{
+        console.log('server is listening on port 80, http://localhost.8000')
+ })
+  
